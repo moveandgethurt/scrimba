4797142d9856e4b2bec675df0786c1e1637f264e
moveandgethurt <moveandgethurt@gmail.com>
2022-11-03 19:30:32 +0200

    finish project6

--- basic javascript/project6_chromeExtension/project6_chromeExtension.js
+++ basic javascript/project6_chromeExtension/project6_chromeExtension.js
@@ -1,25 +1,57 @@
 let myLeads = [];
+
 const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+
+const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    })
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
-    renderLeads();
     inputEl.value = '';
-})
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+    console.log(leadsFromLocalStorage);
+});
 
-function renderLeads() {
-    let listItems = ""
-    for (const lead of myLeads) {
+function render(leads) {
+    let listItems = "";
+    for (const lead of leads) {
         listItems += `
             <li>
                 <a target='_blank' href='${lead}'>
                     ${lead}
                 </a>
             </li>
-        `
+        `;
         // OR:
         // const li = document.createElement("li");
         // li.textContent = inputEl.value;
