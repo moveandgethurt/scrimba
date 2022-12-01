00fd3df8d77b05a81ca1c7acbe9eea62d767164b
moveandgethurt <moveandgethurt@gmail.com>
2022-12-01 18:11:55 +0200

    fix js

--- fe career path/m5_project2_memePicker/m5_project2_memePicker.js
+++ fe career path/m5_project2_memePicker/m5_project2_memePicker.js
@@ -1,12 +1,24 @@
-import { catsData } from '/data.js'
+import { catsData } from './data.js';
 
 const emotionRadios = document.getElementById('emotion-radios');
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio');
+    for (let radio of radios) {
+        radio.classList.remove('highlight');
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight');
+}
 
 function getEmotionsArray(cats){
     const emotionsArr = [];
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArr.push(emotion);
+            if (!emotionsArr.includes(emotion)) {
+                emotionsArr.push(emotion)
+            }
         } 
     }
     return emotionsArr;
@@ -16,8 +28,19 @@ function renderEmotionsRadios(cats){
     const emotions = getEmotionsArray(cats);
     let radioItems = '';
     for (let emotion of emotions) {
-        radioItems += `<p>${emotion}</p>`;
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input 
+                type="radio" 
+                id="${emotion}"
+                value="${emotion}"
+                name="emotion-radios"
+                >
+        </div>
+        `;
     }
     emotionRadios.innerHTML = radioItems;
 }
 renderEmotionsRadios(catsData)
+
