383245f17d301057515bdccae39184f77afa9461
moveandgethurt <moveandgethurt@gmail.com>
2022-12-05 22:54:24 +0200

    add js file w data

---
+++ fe career path/m4_project2_oldagram/m4_project2_oldagram_data.js
@@ -0,0 +1,65 @@
+export const icons = [
+    {
+        name: "iconHeartEl",
+        normal: "images/icon-heart.png",
+        hover: "images/icon-heart-hover.png",
+        click: "images/icon-heart-click.png"
+    },
+    {
+        name: "iconCommentEl",
+        normal: "images/icon-comment.png",
+        hover: "images/icon-comment-hover.png",
+        click: "images/icon-comment-click.png"
+    },
+    {
+        name: "iconDmEl",
+        normal: "images/icon-dm.png",
+        hover: "images/icon-dm-hover.png",
+        click: "images/icon-dm-click.png"
+    }
+]
+
+export const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "avatar-vangogh.jpg",
+        post: "post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21,
+        wiki: 'https://en.wikipedia.org/wiki/Vincent_van_Gogh',
+        map: 'https://goo.gl/maps/oFzNgCVakYqL4kiF8',
+        likeIcon: icons[0],
+        commentIcon: icons[1],
+        dmIcon: icons[2]
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "avatar-courbet.jpg",
+        post: "post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4,
+        wiki: 'https://en.wikipedia.org/wiki/Gustave_Courbet',
+        map: 'https://goo.gl/maps/wXxSPSTmYjxKEM8y9',
+        likeIcon: icons[0],
+        commentIcon: icons[1],
+        dmIcon: icons[2]
+    },
+    {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "avatar-ducreux.jpg",
+        post: "post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152,
+        wiki: 'https://en.wikipedia.org/wiki/Joseph_Ducreux',
+        map: 'https://goo.gl/maps/b4ocBQB6XFS88Qkm8',
+        likeIcon: icons[0],
+        commentIcon: icons[1],
+        dmIcon: icons[2]
+    }
+]
