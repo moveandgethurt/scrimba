a90fc49528e81981077b7ff42045f372b51b8905
moveandgethurt <moveandgethurt@gmail.com>
2022-12-10 15:51:17 +0200

    add functions to render menu

--- fe career path/m5_project4_restaurantMenu/m5_project4_restaurantMenu.js
+++ fe career path/m5_project4_restaurantMenu/m5_project4_restaurantMenu.js
@@ -1,3 +1,28 @@
-import {menuArray} from "./m5_project4_restaurantMenu_data";
+import { menuArray } from './m5_project4_restaurantMenu_data.js';
 
+console.log(menuArray)
 
+function getMenuHtml(menuArray) {
+    let menuHtml = ``;
+    menuArray.forEach(function(item){
+        menuHtml += `
+        <div class="item">
+            <img src="${item.pic}" class="item-pic" alt="${item.name}">
+            <div class="item-description">
+                <p class="item-name">${item.name}</p>
+                <p class="item-ingredients">${item.ingredients}</p>
+                <p class="item-price">$${item.price}</p>
+            </div>
+            <button class="increment-btn" id="increment-btn">+</button>
+        </div>
+        <img src="./images/divider-gray.png" class="divider-gray" alt="divider-gray">
+        `;
+    })
+    return menuHtml;
+}
+
+function render(){
+    document.getElementById('menu').innerHTML = getMenuHtml(menuArray);
+}
+
+render();
