6118e70c21cbda27c63ddd972d7712be599f1500
moveandgethurt <moveandgethurt@gmail.com>
2022-12-12 23:30:36 +0200

    refactor ingredients and add uuids

--- fe career path/m5_project4_restaurantMenu/m5_project4_restaurantMenu_data.js
+++ fe career path/m5_project4_restaurantMenu/m5_project4_restaurantMenu_data.js
@@ -1,23 +1,23 @@
 export const menuArray = [
     {
         name: "Pizza",
-        ingredients: ["pepperoni, mushrooms, mozzarella"],
-        id: 0,
+        ingredients: ["pepperoni", "mushrooms", "mozzarella"],
         price: 14,
-        pic: "./images/pizza.png"
+        pic: "./images/pizza.png",
+        uuid: "005d68dc-3331-4bad-8f39-3ad01905effc",
     },
     {
         name: "Hamburger",
-        ingredients: ["beef, cheese, lettuce"],
+        ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
         pic: "./images/hamburger.png",
-        id: 1
+        uuid: "6e561b57-daf4-46dd-8c96-6caabc73d280",
     },
         {
         name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
+        ingredients: ["grain", "hops", "yeast", "water"],
         price: 12,
         pic: "./images/beer.png",
-        id: 2
+        uuid: "021426ca-c0ba-49af-a396-19bccd285b33",
     }
 ]
