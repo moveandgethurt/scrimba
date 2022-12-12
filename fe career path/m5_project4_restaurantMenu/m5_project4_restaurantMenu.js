4c67cf4460be3a380befa8a3ffee5bb2f43128e7
moveandgethurt <moveandgethurt@gmail.com>
2022-12-12 23:29:56 +0200

    fix the user flow

--- fe career path/m5_project4_restaurantMenu/m5_project4_restaurantMenu.js
+++ fe career path/m5_project4_restaurantMenu/m5_project4_restaurantMenu.js
@@ -1,6 +1,89 @@
 import { menuArray } from './m5_project4_restaurantMenu_data.js';
 
-console.log(menuArray)
+const modal = document.getElementById('modal');
+const order = document.getElementById('order');
+const orderHeading = document.getElementById('order-heading');
+const orderItemsList = document.getElementById('order-items-list');
+const orderTotal = document.getElementById('order-total');
+const thanks = document.getElementById('thanks');
+const paymentForm = document.getElementById('payment-form');
+
+// listen for clicks
+
+document.addEventListener('click', function(e){
+    if (e.target.id === 'complete-btn') {
+        modal.style.display = 'block';
+    }
+    else if (e.target.dataset.increment) {
+        handleIncrementBtn(e.target.dataset.increment);
+    }
+    else if (e.target.dataset.remove) {
+        console.log(e.target.dataset.remove);
+    }
+})
+
+// handle btns
+
+let orderArray = [];
+
+function handleIncrementBtn(itemUUID){
+    const targetItemObj = menuArray.filter(item => item.uuid === itemUUID)[0];
+    orderArray.push(targetItemObj);
+    
+    let counts = orderArray.reduce((map, targetItemObj) => {map[targetItemObj.name] = (map[targetItemObj.name] || 0)+1; return map}, {} );  
+    let orders = [];
+
+    orderArray.forEach(function(order){
+        let item = `
+        <div class="order-container">
+            <div class="item-name-btn">
+                <p class="item-name">${order.name} +${counts[order.name]}</p>
+                <button class="remove-btn"
+                data-remove="${order.uuid}">remove</button>
+            </div>
+            <p class="item-price">$${order.price}</p>
+        </div>
+        `;
+        
+        if(!orders.includes(item)){
+            orders.push(item);
+        }
+    });
+
+    orderItemsList.innerHTML = orders.join('');
+
+    console.log(counts)
+    console.log(orderArray)
+
+    orderHeading.style.display = 'block';
+    orderTotal.innerHTML = `
+    <img src="./images/divider-black.png" class="divider-black" alt="divider-black">
+    <div class="order-container total">
+        <p class="total-heading">Total price:</p>
+        <p class="total-price">$26</p>
+    </div>
+    <button class="complete-btn" id="complete-btn">Complete order</button>
+    `;
+}
+
+// submit form
+
+paymentForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const paymentFormData = new FormData(paymentForm);
+    const name = paymentFormData.get('userName');
+
+    modal.style.display = 'none';
+    order.style.display = 'none';
+    thanks.style.display = 'flex';
+
+    thanks.innerHTML = `
+    <p>Thanks, <span id="user-name">${name}</span>! Your order is on its way! :)</p>
+    `;
+})
+
+// render the menu
 
 function getMenuHtml(menuArray) {
     let menuHtml = ``;
@@ -10,10 +93,12 @@ function getMenuHtml(menuArray) {
             <img src="${item.pic}" class="item-pic" alt="${item.name}">
             <div class="item-description">
                 <p class="item-name">${item.name}</p>
-                <p class="item-ingredients">${item.ingredients}</p>
+                <p class="item-ingredients">${item.ingredients.join(', ')}</p>
                 <p class="item-price">$${item.price}</p>
             </div>
-            <button class="increment-btn" id="increment-btn">+</button>
+            <button class="increment-btn" 
+            id="increment-btn"
+            data-increment="${item.uuid}">+</button>
         </div>
         <img src="./images/divider-gray.png" class="divider-gray" alt="divider-gray">
         `;
