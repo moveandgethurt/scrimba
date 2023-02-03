7375067a43d27d9bdc3e9167df8cb7f0dd1dc7c0
moveandgethurt <moveandgethurt@gmail.com>
2023-02-03 19:35:28 +0200

    add constructor function

--- fe career path/m7_project1_RPG/m7_project1_RPG.js
+++ fe career path/m7_project1_RPG/m7_project1_RPG.js
@@ -1,47 +1,12 @@
-const hero = {
-    elementId: 'hero',
-    name: 'Wizard',
-    avatar: 'images/wizard.png',
-    health: '60',
-    diceCount: 3
+import characterData from "./m7_project1_RPG_data.js";
+import Character from "./m7_project1_RPG_Character.js";
+
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml();    
 }
 
-const monster = {
-    elementId: 'monster',
-    name: 'Orc',
-    avatar: 'images/orc.png',
-    health: '10',
-    diceCount: 1
-}
-
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function() {
-        return Math.floor(Math.random()*6) + 1;
-    });
-}
-
-function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount).map(function(num) {
-        return `<div class="dice">${num}</div>`
-    }).join('');
-}
-
-
-function renderCharacter(data) {
-    const {elementId, name, avatar, health, diceCount} = data;
-
-    const diceHtml = getDiceHtml(diceCount);
-
-    document.getElementById(elementId).innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container">
-            ${diceHtml}
-        </div>
-    </div>`;
-}
-
-renderCharacter(hero);
-renderCharacter(monster);
+render()
