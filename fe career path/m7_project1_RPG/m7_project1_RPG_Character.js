c56cde25a2d0859e6f63e82edc9876a210c1cc5b
moveandgethurt <moveandgethurt@gmail.com>
2023-02-04 15:00:56 +0200

    modify constructor function to make attack button work

--- fe career path/m7_project1_RPG/m7_project1_RPG_Character.js
+++ fe career path/m7_project1_RPG/m7_project1_RPG_Character.js
@@ -1,24 +1,26 @@
-import { getDiceRollArray } from "./m7_project1_RPG_utils.js";
+import { getDiceRollArray, getDicePlaceholderHtml } from "./m7_project1_RPG_utils.js";
 
 function Character(data) {
     Object.assign(this, data);
 
+    this.diceArray = getDicePlaceholderHtml(this.diceCount);
+
     this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num) {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceArray = this.currentDiceScore.map(function(num) {
             return `<div class="dice">${num}</div>`
         }).join('');
-    };
+    } 
 
     this.getCharacterHtml = function() {
-        const {elementId, name, avatar, health, diceCount} = this;
-        let diceHtml = this.getDiceHtml(diceCount);
+        const {elementId, name, avatar, health, diceCount, diceArray} = this;
 
         return `<div class="character-card">
                     <h4 class="name"> ${name} </h4>
                     <img class="avatar" src="${avatar}"/>
                     <p class="health">health: <b> ${health} </b></p>
                     <div class="dice-container">
-                        ${diceHtml}
+                        ${diceArray}
                     </div>
                 </div>`;
     }
