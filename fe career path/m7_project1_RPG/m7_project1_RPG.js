9da5722eeab18fe8e29614322309ac75fcc95d66
moveandgethurt <moveandgethurt@gmail.com>
2023-02-07 21:08:02 +0200

    finish m7_project1

--- fe career path/m7_project1_RPG/m7_project1_RPG.js
+++ fe career path/m7_project1_RPG/m7_project1_RPG.js
@@ -1,39 +1,68 @@
-import characterData from "./m7_project1_RPG_data.js";
-import Character from "./m7_project1_RPG_Character.js";
+import characterData from './m7_project1_RPG_data.js'
+import Character from './m7_project1_RPG_Character.js'
+
+let monstersArray = ["orc", "demon", "goblin"]
+
+function getNewMonster() {
+    const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
+}
 
 function attack() {
-    wizard.getDiceHtml();
-    orc.getDiceHtml();
-    wizard.takeDamage(orc.currentDiceScore);
-    orc.takeDamage(wizard.currentDiceScore);
-    if (wizard.dead || orc.dead) {
+    wizard.setDiceHtml()
+    monster.setDiceHtml()
+    wizard.takeDamage(monster.currentDiceScore)
+    monster.takeDamage(wizard.currentDiceScore)
+    render()
+
+    if (wizard.dead) {
         endGame();
+    } else if (monster.dead) {
+        if (monstersArray.length > 0) {
+                document.getElementById('attack-button').disabled = true;
+                setTimeout(()=>{
+                    document.getElementById('attack-button').disabled = false;
+                    monster = getNewMonster();
+                    render()
+                }, 1500);
+                
+        } else {
+            document.getElementById('attack-button').disabled = true;
+            endGame();
+        }
     }
-    render();
 }
 
 function endGame() {
-    const endMessage = wizard.health === 0 && orc.health === 0
-                    ? "No victors - all creatures are dead" 
-                    : wizard.health > 0 ? "The Wizard Wins" 
-                    : "The Orc is Victorious";
-    const endEmoji = wizard.health > 0 ? "🔮" : "☠️";
-    document.body.innerHTML = 
-                    `<div class="end-game">
-                        <h2>Game Over</h2>
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+            "The Monsters are Victorious"
+
+    const endEmoji = wizard.health > 0 ? "❤️" : "☠️"
+    
+    setTimeout(()=>{
+        document.body.innerHTML = `
+                    <div class="end-game">
+                        <h2>Game Over</h2> 
                         <h3>${endMessage}</h3>
                         <p class="end-emoji">${endEmoji}</p>
-                    </div>`;
+                        <button type="button" onClick="window.location.reload()">
+                            Try again!
+                        </button>
+                    </div>
+                    `
+        }, 1500)
+    
 }
+
+document.getElementById("attack-button").addEventListener('click', attack)
 
 function render() {
-    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
-    document.getElementById('monster').innerHTML = orc.getCharacterHtml();    
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = monster.getCharacterHtml()
 }
 
-document.getElementById("attack-button").addEventListener("click", attack);
-
-const wizard = new Character(characterData.hero);
-const orc = new Character(characterData.monster);
-
+const wizard = new Character(characterData.hero)
+let monster = getNewMonster()
 render()
