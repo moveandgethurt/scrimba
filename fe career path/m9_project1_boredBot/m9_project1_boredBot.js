61628ae7e6391aded7d40d5bff3cba33fd41f81b
moveandgethurt <moveandgethurt@gmail.com>
2023-02-13 13:55:14 +0200

    add completed m9_project1

---
+++ fe career path/m9_project1_boredBot/m9_project1_boredBot.js
@@ -0,0 +1,10 @@
+document.getElementById('bored-bot').addEventListener('click', function(){
+    fetch('https://apis.scrimba.com/bored/api/activity')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('idea').textContent = data.activity;
+            document.getElementById('title').textContent = '! HappyBot !';
+        document.body.classList.add('happy-body');
+        document.getElementById('bored-bot').classList.add('happy-button');
+        })  
+})
