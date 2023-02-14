6faf580e24bca08e5429c6c6013cf802a2acf8c5
moveandgethurt <moveandgethurt@gmail.com>
2023-02-14 18:16:44 +0200

    create renderPosts() and form.addEventListener

--- fe career path/m9_project2_blogSpace/m9_project2_blogSpace.js
+++ fe career path/m9_project2_blogSpace/m9_project2_blogSpace.js
@@ -1,13 +1,48 @@
+let postsArr = [];
+const postTitle = document.getElementById('post-title');
+const postBody = document.getElementById('post-body');
+const form = document.getElementById('new-post');
+
+function renderPosts() {
+    let postsHtml = '';
+    postsArr.forEach(function(post){
+        postsHtml += `
+        <div class="post-container">
+            <h3 class="post-title">${post.title}</h3>
+            <p class="post-body">${post.body}</p>
+        </div>
+    `;
+    })
+    document.getElementById('blog-posts').innerHTML = postsHtml;
+}
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        postsArr.forEach(function(post){
-            document.getElementById('blog-posts').innerHTML += `
-                <div class="post-container">
-                    <h3 class="post-title">${post.title}</h3>
-                    <p class="post-body">${post.body}</p>
-                </div>
-            `;
-        })
+        postsArr = data.slice(0, 5)
+        renderPosts();
+})
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    const post = {
+        title: postTitle.value,
+        body: postBody.value
+    }
+    fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+        method: 'POST',
+        body: JSON.stringify(post),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
+        .then(res => res.json())
+        .then(post => {
+            postsArr.unshift(post);
+            renderPosts()
+            form.reset();
+        })
+})
+
+
+
