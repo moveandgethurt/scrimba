8db2301a1129ee567564954cd6c4031f7387c2f7
moveandgethurt <moveandgethurt@gmail.com>
2023-02-19 15:02:08 +0200

    add new variables, edit existing functons, add getWinner()

--- fe career path/m9_project4_warGame/m9_project4_warGame.js
+++ fe career path/m9_project4_warGame/m9_project4_warGame.js
@@ -1,14 +1,33 @@
-let deckId
+let deckId;
+let card1score = 0;
+let card2score = 0;
+
 const newDeckBtn = document.getElementById('new-deck');
 const drawCardsBtn = document.getElementById('draw-cards');
-const card1 = document.getElementById('card-1');
-const card2 = document.getElementById('card-2');
+const card1el = document.getElementById('card-1');
+const card2el = document.getElementById('card-2');
+const remainingEl = document.getElementById('remaining');
+const deckIdEL = document.getElementById('deck-id');
+const titleEl = document.getElementById('title');
+const computerScoreEl = document.getElementById('computer-score');
+const myScoreEl = document.getElementById('my-score');
+
+newDeckBtn.addEventListener('click', newDeck);
+drawCardsBtn.addEventListener('click', drawCards);
 
 function newDeck() {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
         .then(res => res.json())
         .then(data => {
             deckId = data.deck_id;
+            remainingEl.textContent = `${data.remaining}`;
+            deckIdEL.textContent = `deck id: ${deckId}`
+            titleEl.textContent = 'Game of War';
+            card1score = 0;
+            card2score = 0;
+            computerScoreEl.textContent = card1score;
+            myScoreEl.textContent = card2score;
+            drawCardsBtn.disabled = false;
         })
 }
 
@@ -17,11 +36,35 @@ function drawCards() {
         fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
             .then(res => res.json())
             .then(data => {
-                card1.innerHTML = `<img src="${data.cards[0].image}">`;
-                card2.innerHTML = `<img src="${data.cards[1].image}">`;
+                card1el.innerHTML = `<img src="${data.cards[0].image}">`;
+                card2el.innerHTML = `<img src="${data.cards[1].image}">`;
+                titleEl.textContent = getWinner(data.cards[0], data.cards[1]);
+                remainingEl.textContent = `${data.remaining}`;
+                drawCardsBtn.disabled = false;
+                if (data.remaining === 0) {
+                    drawCardsBtn.disabled = true;
+                    titleEl.textContent = card1score > card2score ? 
+                    'Computer wins the game!' : card2score > card1score ? 
+                    'You win the game!' : `It's a complete war!`;
+                } 
             })
     } else { alert(`Click 'New Deck' first to draw new cards!`) }
 }
 
-newDeckBtn.addEventListener('click', newDeck)
-drawCardsBtn.addEventListener('click', drawCards)
+function getWinner(card1, card2) {
+    const valueArr = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1index = valueArr.indexOf(card1.value);
+    const card2index = valueArr.indexOf(card2.value);
+    if (card1index > card2index) {
+        card1score ++;
+        computerScoreEl.textContent = card1score;
+        return 'Computer wins!';
+    } else if (card1index < card2index) {
+        card2score++;
+        myScoreEl.textContent = card2score;
+        return 'You win!';
+    } else {
+        return `War!`;
+    }
+}
+
