c0a2e1e72ac98292f684c099d4a03a894fe6fcfd
moveandgethurt <moveandgethurt@gmail.com>
2023-03-04 21:31:35 +0200

    refactor the file

--- fe career path/m11_project1_react-info-site/src/index.js
+++ fe career path/m11_project1_react-info-site/src/index.js
@@ -1,54 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
-function Header() {
-    return (
-        <header>
-            <nav>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="react icon" />
-                <ul className='navbar'>
-                    <a href="#"><li>Pricing</li></a>
-                    <a href="#"><li>About</li></a>
-                    <a href="#"><li>Contact</li></a>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function MainContent() {
-    return (
-        <div className='main-container'>
-            <h1>Fun facts about React</h1>
-            <ul className='main-list'>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by John Walke</li>
-                <li>Has well over 100K stars on Github</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </div>
-    );
-}
-
-function Footer() {
-    return (
-        <footer>
-            <small>© 2023 Korotkova development. All rights reserved.</small>
-        </footer>
-    );
-}
-
-function Page() {
-    return (
-        <div className='page-container'>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    );
-}
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Page />);
+root.render(<App />);
