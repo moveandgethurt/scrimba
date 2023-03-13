1270756fcac1988cd3f8e9511819482b5b18c3f6
moveandgethurt <moveandgethurt@gmail.com>
2023-03-13 16:55:50 +0200

    add basic logic

--- fe career path/m11_project5_meme-generator/src/components/Meme.js
+++ fe career path/m11_project5_meme-generator/src/components/Meme.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-export default function Form() {
-    function handleClick() {
-        console.log('I was clicked!')
+import memesData from "../memesData";
+
+export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState('')
+
+    function getMemeImage() {
+        const memesArr = memesData.data.memes;
+        const randomNum = Math.floor(Math.random() * memesArr.length);
+        setMemeImage(memesArr[randomNum].url)
     }
 
     return (
@@ -20,8 +26,9 @@ export default function Form() {
                         placeholder="Bottom text here"
                     />
                 </div>
-                <button className="form--btn" onClick={handleClick}>Get a new meme image <i className="fa-regular fa-image form--btn-icon"></i></button>
+                <button className="form--btn" onClick={getMemeImage}>Get a new meme image <i className="fa-regular fa-image form--btn-icon"></i></button>
             </div>
+            <img src={memeImage} alt="random meme" className="meme--image" />
         </main>
     )
 }
