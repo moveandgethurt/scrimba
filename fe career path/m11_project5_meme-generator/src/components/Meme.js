9587c1a2f3043204538c209809f4c3e1a4ced0a1
moveandgethurt <moveandgethurt@gmail.com>
2023-03-14 18:07:42 +0200

    refactor state

--- fe career path/m11_project5_meme-generator/src/components/Meme.js
+++ fe career path/m11_project5_meme-generator/src/components/Meme.js
@@ -3,12 +3,24 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = React.useState('')
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'http://i.imgflip.com/1bij.jpg'
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
-        const memesArr = memesData.data.memes;
+        const memesArr = allMemeImages.data.memes;
         const randomNum = Math.floor(Math.random() * memesArr.length);
-        setMemeImage(memesArr[randomNum].url)
+        const url = memesArr[randomNum].url;
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                randomImage: url
+            }
+        })
     }
 
     return (
@@ -26,9 +38,15 @@ export default function Meme() {
                         placeholder="Bottom text here"
                     />
                 </div>
-                <button className="form--btn" onClick={getMemeImage}>Get a new meme image <i className="fa-regular fa-image form--btn-icon"></i></button>
+                <button 
+                    className="form--btn" 
+                    onClick={getMemeImage}
+                > 
+                    Get a new meme image 
+                    <i className="fa-regular fa-image form--btn-icon"></i>
+                </button>
             </div>
-            <img src={memeImage} alt="random meme" className="meme--image" />
+            <img src={meme.randomImage} alt="random meme" className="meme--image" />
         </main>
     )
 }
