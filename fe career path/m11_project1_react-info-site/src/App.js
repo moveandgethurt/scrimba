e2e5cba74a142e7ee03057a7f9f4d17ce3e39940
moveandgethurt <moveandgethurt@gmail.com>
2023-07-15 15:03:20 +0300

    change all the code and add dark mode

--- fe career path/m11_project1_react-info-site/src/App.js
+++ fe career path/m11_project1_react-info-site/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react'
-
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
 export default function App() {
+    const [darkMode, setDarkMode] = React.useState(false)
+
+    function toggleDarkMode() {
+      setDarkMode(prevMode => !prevMode)
+    }
+
     return (
-        <div className='container'>
-            <Header />
-            <MainContent />
-            <Footer />
+        <div className="container">
+            <Navbar 
+              darkMode={darkMode}
+              toggleDarkMode={toggleDarkMode}
+           />
+            <Main darkMode={darkMode} />
         </div>
-    );
+    )
 }
--- fe career path/m11_project1_react-info-site/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <footer>
-            <small>© 2023 Korotkova development. All rights reserved.</small>
-        </footer>
-    );
-}
